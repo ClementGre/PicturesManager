c467ea2f1de313f8bd4663c4663bfeb4467f89de
Clément Grennerat <clement.grennerat@free.fr>
2023-02-02 22:30:08 +0100

    Setup Alt kay navigation in menu [in progress]
    Still some issues with the focus, and sub-items not yet supported.

--- src/header/menubar.rs
+++ src/header/menubar.rs
@@ -1,14 +1,12 @@
 use crate::{
     header::menu::{get_menus, MenuItem},
     invoke,
-    utils::{
-        keystroke::KeyStroke,
-        logger::{info, tr},
-    },
+    utils::{keystroke::KeyStroke, logger::info},
 };
 use wasm_bindgen::{prelude::Closure, JsCast, JsValue};
 use web_sys::{window, Element, HtmlElement};
 use yew::prelude::*;
+use unidecode::unidecode;
 
 fn register_shortcuts(items: &Vec<MenuItem>, shortcuts: &mut Vec<(KeyStroke, String)>) {
     items.into_iter().for_each(|item| {
@@ -27,6 +25,14 @@ pub fn MenuBar() -> Html {
 
     let mut shortcuts = vec![];
     register_shortcuts(&menus, &mut shortcuts);
+    let mut alt_shortcuts = vec![];
+    menus.iter().for_each(|menu| {
+        let name = menu.name.clone().unwrap_or("".to_string());
+        let split = name.split("_");
+        if split.clone().count() >= 2 {
+            alt_shortcuts.push((unidecode(split.skip(1).next().unwrap().to_lowercase().as_str())[..1].to_string(), menu.id.clone()));
+        }
+    });
 
     let is_open = use_state_eq(|| false);
     let selected = use_state_eq(|| "".to_string());
@@ -34,40 +40,53 @@ pub fn MenuBar() -> Html {
     // GLOBAL EVENTS
     {
         let is_open = is_open.clone();
-        use_memo( |_| {
-
-            let keyboard_event = Closure::wrap(Box::new(move |e: web_sys::KeyboardEvent| {
-                shortcuts.clone().iter().for_each(|(ks, id)| {
-                    if ks.matches(&e) {
-                        invoke(format!("menu_{}", id).as_str(), JsValue::default());
-                    }
-                });
-            }) as Box<dyn FnMut(_)>);
-
-            let _ = window()
-                .unwrap()
-                .add_event_listener_with_callback("keydown", keyboard_event.as_ref().unchecked_ref())
-                .unwrap();
-            keyboard_event.forget(); // Makes a memory leak, but this closure is global and needs to live as long as the window is open
-
-            let mouse_event = {
-                Closure::wrap(Box::new(move |e: web_sys::MouseEvent| {
-                    let target = e.target().and_then(|div| div.dyn_into::<HtmlElement>().ok());
-                    if let Some(div) = target {
-                        if !div.class_name().split_whitespace().any(|c| "menu-item" == c || "menu" == c) {
-                            is_open.set(false); // Close menu only if the target is not a menu-item
+        use_memo(
+            |_| {
+                let keyboard_event = {
+                    let is_open = is_open.clone();
+                    let selected = selected.clone();
+                    Closure::wrap(Box::new(move |e: web_sys::KeyboardEvent| {
+                        shortcuts.clone().iter().for_each(|(ks, id)| {
+                            if ks.matches(&e) {
+                                invoke(format!("menu_{}", id).as_str(), JsValue::default());
+                            }
+                        });
+                        if e.alt_key() {
+                            alt_shortcuts.clone().iter().for_each(|(ks, id)| {
+                                if ks == &unidecode(e.key().to_lowercase().as_str())[..1].to_string() {
+                                    is_open.set(true);
+                                    selected.set(id.clone());
+                                }
+                            });
                         }
-                    }
-                }) as Box<dyn FnMut(_)>)
-            };
+                    }) as Box<dyn FnMut(_)>)
+                };
 
-            let _ = window()
-                .unwrap()
-                .add_event_listener_with_callback("mousedown", mouse_event.as_ref().unchecked_ref())
-                .unwrap();
-            mouse_event.forget(); // Makes a memory leak, but this closure is global and needs to live as long as the window is open
-            info("forgetting mouse event");
-        }, ());
+                let _ = window()
+                    .unwrap()
+                    .add_event_listener_with_callback("keydown", keyboard_event.as_ref().unchecked_ref())
+                    .unwrap();
+                keyboard_event.forget(); // Makes a memory leak, but this closure is global and needs to live as long as the window is open
+
+                let mouse_event = {
+                    Closure::wrap(Box::new(move |e: web_sys::MouseEvent| {
+                        let target = e.target().and_then(|div| div.dyn_into::<HtmlElement>().ok());
+                        if let Some(div) = target {
+                            if !div.class_name().split_whitespace().any(|c| "menu-item" == c || "menu" == c) {
+                                is_open.set(false); // Close menu only if the target is not a menu-item
+                            }
+                        }
+                    }) as Box<dyn FnMut(_)>)
+                };
+
+                let _ = window()
+                    .unwrap()
+                    .add_event_listener_with_callback("mousedown", mouse_event.as_ref().unchecked_ref())
+                    .unwrap();
+                mouse_event.forget(); // Makes a memory leak, but this closure is global and needs to live as long as the window is open
+            },
+            (),
+        );
     }
 
     let on_bar_click = {
@@ -128,12 +147,24 @@ fn MenuItemComponent(props: &MenuItemProps) -> Html {
             selected.set(id.clone())
         })
     };
+    let on_mouse_leave = {
+        let menu_ref = menu_ref.clone();
+        let item_ref = item_ref.clone();
+        Callback::from(move |_| {
+            if let Some(menu) = menu_ref.cast::<HtmlElement>() {
+                menu.blur().unwrap();
+            } else {
+                item_ref.cast::<HtmlElement>().unwrap().blur().unwrap();
+            }
+        })
+    };
     let on_key_press = {
         let selected = selected.clone();
         let id = menu.id.clone();
         let is_menu = menu.items.is_some();
         let is_root = is_root.clone();
         let is_open = is_open.clone();
+        let menu_ref = menu_ref.clone();
         let item_ref = item_ref.clone();
         Callback::from(move |e: KeyboardEvent| {
             // Enter key
@@ -143,10 +174,14 @@ fn MenuItemComponent(props: &MenuItemProps) -> Html {
                 if is_menu {
                     if *selected == id.clone() {
                         selected.set("".to_string());
-                        if is_root { is_open.clone().set(false); }
-                    }else{
+                        if is_root {
+                            is_open.clone().set(false);
+                        }
+                    } else {
                         selected.set(id.clone());
-                        if is_root { is_open.clone().set(true); }
+                        if is_root {
+                            is_open.clone().set(true);
+                        }
                     }
                 } else {
                     invoke(format!("menu_{}", id).as_str(), JsValue::default());
@@ -154,13 +189,24 @@ fn MenuItemComponent(props: &MenuItemProps) -> Html {
                     item_ref.clone().cast::<HtmlElement>().unwrap().blur().unwrap();
                 }
             }
+            // Escape key
+            else if e.key_code() == 27 {
+                e.prevent_default();
+                e.stop_propagation();
+                is_open.clone().set(false);
+                if let Some(menu) = menu_ref.cast::<HtmlElement>() {
+                    menu.blur().unwrap();
+                } else {
+                    item_ref.cast::<HtmlElement>().unwrap().blur().unwrap();
+                }
+            }
         })
     };
 
     let on_focus = {
         let selected = selected.clone();
-        Callback::from(move |_| {
-            selected.set("".to_string());
+        Callback::from(move |_: _| {
+            //selected.set("".to_string());
         })
     };
 
@@ -184,6 +230,29 @@ fn MenuItemComponent(props: &MenuItemProps) -> Html {
         });
     }
 
+    {
+        // let selected = selected.clone();
+        // let menu_ref = menu_ref.clone();
+        // let item_ref = item_ref.clone();
+        // let id = menu.id.clone();
+        // let is_menu = menu.items.is_some();
+        // use_effect(move || {
+        //     if is_menu && *selected == id {
+        //         if let Some(menu) = menu_ref.cast::<HtmlElement>() {
+        //             if menu_ref.cast::<Element>() != window().unwrap().document().unwrap().active_element() {
+        //                 info(format!("focusing menu {}", id).as_str());
+        //                 menu.focus().unwrap();
+        //             }else {
+        //                 info("menu already focused");
+        //             }
+                    
+        //         } else if let Some(item) = item_ref.cast::<HtmlElement>() {
+        //             item.focus().unwrap();
+        //         }
+        //     }
+        // });
+    }
+
     if let Some(items) = menu.items {
         html! {
             <div key={menu.id.clone()}
@@ -192,8 +261,10 @@ fn MenuItemComponent(props: &MenuItemProps) -> Html {
                 tabindex="0"
                 onfocus={on_focus}
                 onkeydown={on_key_press}
-                onmouseenter={on_mouse_enter}>
-                <p>{{menu.name.clone()}}</p>
+                onmouseenter={on_mouse_enter}
+                onmouseleave={on_mouse_leave}>
+
+                <MenuTextComponent text={menu.name.clone().unwrap()} />
                 {
                     if !is_root {
                         html! { <div class="menu-arrow"><div></div></div> }
@@ -220,7 +291,6 @@ fn MenuItemComponent(props: &MenuItemProps) -> Html {
             </div>
         }
     } else if let Some(name) = menu.name {
-
         let on_click = {
             let event = format!("menu_{}", menu.id);
             let item_ref = item_ref.clone();
@@ -239,9 +309,11 @@ fn MenuItemComponent(props: &MenuItemProps) -> Html {
                 onclick={on_click}
                 onfocus={on_focus}
                 onkeydown={on_key_press}
-                onmouseenter={on_mouse_enter}>
+                onmouseenter={on_mouse_enter}
+                onmouseleave={on_mouse_leave}>
 
-                <p>{name}</p>
+                <MenuTextComponent text={name} />
+
                 {
                     if menu.accelerator.is_some() {
                         html!{
@@ -260,5 +332,44 @@ fn MenuItemComponent(props: &MenuItemProps) -> Html {
                 <hr />
             </div>
         }
+    }
+}
+
+#[derive(Clone, Properties, PartialEq)]
+struct MenuTextProps {
+    text: String,
+}
+
+#[function_component]
+fn MenuTextComponent(props: &MenuTextProps) -> Html {
+    let text = props.text.clone();
+
+    let mut left_part = String::new();
+    let mut shortcut = String::new();
+    let mut right_part = String::new();
+
+    let mut is_shortcut = false;
+
+    for c in text.chars() {
+        if c == '_' {
+            is_shortcut = true;
+            left_part = right_part;
+            right_part = String::new();
+        } else {
+            if is_shortcut {
+                shortcut = c.to_string();
+                is_shortcut = false;
+            } else {
+                right_part.push(c);
+            }
+        }
+    }
+
+    html! {
+        <p>
+            {left_part}
+            <span>{shortcut}</span>
+            {right_part}
+        </p>
     }
 }
