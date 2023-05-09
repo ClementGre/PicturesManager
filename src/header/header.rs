1ed1b2acd13580c738bfeafa3e78762433804311
Clément Grennerat <clement.grennerat@free.fr>
2023-05-09 20:07:05 +0200

    Setup window title and buttons scroll shadow

--- src/header/header.rs
+++ src/header/header.rs
@@ -10,7 +10,8 @@ pub struct Props {
 
 #[function_component]
 pub fn Header(props: &Props) -> Html {
-    let macos = use_context::<Context>().unwrap().macos;
+    let mut macos = use_context::<Context>().unwrap().macos;
+    macos = false;
 
     let on_minimize = Callback::from(move |_: MouseEvent| {
         invoke("window_minimize", JsValue::default());
@@ -30,7 +31,13 @@ pub fn Header(props: &Props) -> Html {
                 {
                     if macos {
                         html! {
-                            <div class="macos-spacer" data-tauri-drag-region="true"/>
+                            <>
+                                <div class="macos-spacer" data-tauri-drag-region="true"/>
+                                <div class="title" data-tauri-drag-region="true">
+                                    <p class="title" data-tauri-drag-region="true">{"PictureFiler"}</p>
+                                    <p class="path" data-tauri-drag-region="true">{"~/Downloads/Gallery/"}</p>
+                                </div>
+                            </>
                         }
                     } else {
                         html! {
@@ -46,11 +53,53 @@ pub fn Header(props: &Props) -> Html {
                 }
                 // Macos buttons are right aligned, but windows buttons are left aligned
                 <div class={ if macos {"spacer"} else { "fixed-spacer" }} data-tauri-drag-region="true"/>
-                
-                // Some buttons are only available on Macos because on Windows, the menu bar is more accessible
+
+
                 <div class="buttons" data-tauri-drag-region="true">
-                    <div class="button" role="button" aria-labelledby="Star">
+                    {
+                        // Some buttons are only available on Macos because on Windows, the menu bar is more accessible
+                        if macos {
+                            html! {
+                                <>
+                                    <div class="button" tabindex="0" role="button" aria-labelledby="Star">
+                                        <i class="fa-regular fa-star"></i>
+                                    </div>
+                                    <div class="button" tabindex="0" role="button" aria-labelledby="Star">
+                                        <i class="fa-solid fa-sidebar"></i>
+                                    </div>
+                                    <div class="button" tabindex="0" role="button" aria-labelledby="Star">
+                                        <i class="fa-brands fa-twitter"></i>
+                                    </div>
+                                    <div class="button" tabindex="0" role="button" aria-labelledby="Star">
+                                        <i class="fa-brands fa-twitter"></i>
+                                    </div>
+                                    <div class="button" tabindex="0" role="button" aria-labelledby="Star">
+                                        <i class="fa-brands fa-twitter"></i>
+                                    </div>
+                                    <div class="button" tabindex="0" role="button" aria-labelledby="Star">
+                                        <i class="fa-brands fa-twitter"></i>
+                                    </div>
+                                </>
+                            }
+                        }else{ html!() }
+                    }
+                    <div class="button" tabindex="0" role="button" aria-labelledby="Star">
                         <i class="fa-regular fa-star"></i>
+                    </div>
+                    <div class="button" tabindex="0" role="button" aria-labelledby="Star">
+                        <i class="fa-solid fa-sidebar"></i>
+                    </div>
+                    <div class="button" tabindex="0" role="button" aria-labelledby="Star">
+                        <i class="fa-brands fa-twitter"></i>
+                    </div>
+                    <div class="button" tabindex="0" role="button" aria-labelledby="Star">
+                        <i class="fa-brands fa-twitter"></i>
+                    </div>
+                    <div class="button" tabindex="0" role="button" aria-labelledby="Star">
+                        <i class="fa-brands fa-twitter"></i>
+                    </div>
+                    <div class="button" tabindex="0" role="button" aria-labelledby="Star">
+                        <i class="fa-brands fa-twitter"></i>
                     </div>
                 </div>
 
@@ -58,15 +107,20 @@ pub fn Header(props: &Props) -> Html {
                      if !macos {
                         html! {
                             <>
+                                <div class="fixed-spacer" data-tauri-drag-region="true"/>
+                                <div class="title" data-tauri-drag-region="true">
+                                    <p class="title" data-tauri-drag-region="true">{"PictureFiler"}</p>
+                                    <p class="path" data-tauri-drag-region="true">{"~/Downloads/Gallery/"}</p>
+                                </div>
                                 <div class="spacer" data-tauri-drag-region="true"/>
                                 <div class="windows-buttons" data-tauri-drag-region="true">
-                                    <div class="minimize" onclick={on_minimize} role="button" aria-labelledby="Minimize window">
+                                    <div class="minimize" onclick={on_minimize} tabindex="0" role="button" aria-labelledby="Minimize window">
                                         <div></div>
                                     </div>
-                                    <div class="maximize" onclick={on_maximize} role="button" aria-labelledby="Maximize window">
+                                    <div class="maximize" onclick={on_maximize} tabindex="0" role="button" aria-labelledby="Maximize window">
                                         <div></div>
                                     </div>
-                                    <div class="close" onclick={on_close} role="button" aria-labelledby="Close window">
+                                    <div class="close" onclick={on_close} tabindex="0" role="button" aria-labelledby="Close window">
                                         <div class="first"></div>
                                         <div class="second"></div>
                                     </div>
