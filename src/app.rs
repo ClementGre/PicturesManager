228f0cbda2e748c56a0075c73de94602fd330c35
Clément GRENNERAT <clement.grennerat@free.fr>
2023-05-13 16:34:36 +0200

    Setup right implementation of the App component, with correct management of Settings and Context contexts.

--- src/app.rs
+++ src/app.rs
@@ -1,25 +1,25 @@
-use crate::utils::logger::*;
-use crate::utils::utils::{cmd_async, cmd_async_get};
-use futures::stream::StreamExt;
-use pm_common::data_structs::Theme;
-use serde::{Deserialize, Serialize};
-use tauri_sys::event::listen;
-use tauri_sys::os::{self, OsKind};
-use tauri_sys::window;
-use yew::platform::spawn_local;
-use yew::prelude::*;
-use yew::suspense::use_future;
-
 use crate::header::header::Header;
 use crate::leftbar::leftbar::LeftBar;
 use crate::mainpane::mainpane::MainPane;
 use crate::rightbar::rightbar::RightBar;
+use crate::utils::logger::*;
 use crate::utils::translator::Translator;
+use crate::utils::utils::cmd_async_get;
+use futures::stream::StreamExt;
+use pm_common::app_data::{Settings, Theme};
+use serde::{Deserialize, Serialize};
+use tauri_sys::event::listen;
+use tauri_sys::os::{self, OsKind};
+use tauri_sys::window::{self, current_window};
+use yew::platform::spawn_local;
+use yew::prelude::*;
+use yew::suspense::use_future;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Context {
     pub macos: bool,
     pub windows: bool,
+    pub theme: Theme,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -29,46 +29,77 @@ pub struct GreetArgs<'a> {
 
 #[function_component]
 pub fn App() -> HtmlResult {
-    let lang = use_future(|| async { cmd_async_get::<String>("get_language").await })?;
-    tr((*lang).as_str());
 
-    let theme = use_future(|| async { cmd_async_get::<Theme>("get_theme_or_os").await })?;
-    let theme = use_state(|| *theme);
-    let os = use_future(|| async { os::kind().await.unwrap_or(OsKind::Linux) })?;
+    // OS
+    let os = use_future(|| async {
+        os::kind().await.unwrap_or(OsKind::Linux)
+    })?;
 
-    /* Context définition */
-    let context = {
-        use_state(|| Context {
-            macos: *os == OsKind::Darwin,
-            windows: *os == OsKind::WindowsNT,
-        })
-    };
+    // Settings got with use_future saved in state
+    let settings_future = use_future(|| async { cmd_async_get::<Settings>("get_settings").await })?;
+    let settings = use_state(|| settings_future.clone());
 
-    let translator = Translator::new("fr-FR".parse().expect("Invalid language identifier"));
-
-    /* OS theme sync */
+    // OS theme got with use_future and might be updated with event listener
+    let os_theme_future = use_future(|| async {
+        current_window().theme().await.unwrap()
+    })?;
+    let os_theme = use_state(|| os_theme_future.clone());
     spawn_local({
-        let theme = theme.clone();
+        let os_theme = os_theme.clone();
         async move {
             let mut events = listen::<tauri_sys::window::Theme>("tauri://theme-changed").await.unwrap();
             while let Some(e) = events.next().await {
-                if cmd_async::<(), bool>("is_system_theme", &()).await {
-                    theme.set(if e.payload == window::Theme::Light { Theme::LIGHT } else { Theme::DARK });
-                }
+                os_theme.set(e.payload);
             }
         }
     });
 
+    // Theme memo calculated based on settings and os_theme
+    let theme = {
+        let settings = settings.clone();
+        let os_theme = os_theme.clone();
+        use_memo(move |(settings, os_theme)| {
+            if settings.theme == Theme::System {
+                if **os_theme == window::Theme::Light {
+                    Theme::Light
+                } else {
+                    Theme::Dark
+                }
+            } else {
+                settings.theme
+            }
+        }, (settings, os_theme))
+    };
+
+    
+    // Language and translations
+    let lang = use_future(|| async { cmd_async_get::<String>("get_language").await })?;
+    tr((*lang).as_str());
+
+    let translator = Translator::new("fr-FR".parse().expect("Invalid language identifier"));
+
+    // Context définition
+    let context = {
+        let os = os.clone();
+        let theme = theme.clone();
+        use_memo(|(os, theme)| Context {
+            macos: *os == OsKind::Darwin,
+            windows: *os == OsKind::WindowsNT,
+            theme: *theme.clone(),
+        }, (os, theme))
+    };
+
     Ok(html! {
         <>
             <ContextProvider<Context> context={(*context).clone()}>
-                <Header class={if *theme == Theme::LIGHT { "th-light" } else { "th-dark" }}/>
-                <main class="light">
-                    <LeftBar/>
-                    <MainPane/>
-                    <RightBar/>
-                </main>
-
+                <ContextProvider<Settings> context={(*settings).clone()}>
+                    <Header class={if *theme == Theme::Light { "th-light" } else { "th-dark" }}/>
+                    <main class="light">
+                        <LeftBar/>
+                        <MainPane/>
+                        <RightBar/>
+                    </main>
+                </ContextProvider<Settings>>
             </ContextProvider<Context>>
         </>
     })
